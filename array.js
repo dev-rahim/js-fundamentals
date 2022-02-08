4d66dec93321a3814175718940665716719816ea
dev-rahim <rahimuddin5915@gmail.com>
2022-02-08 21:10:46 +0600

    Added array.js

---
+++ array.js
@@ -0,0 +1,10 @@
+// number Array
+var friendsAge = [ 11, 13, 15, 52, 52, 55 ];
+console.log( friendsAge[ 2 ] );
+
+// string Array 
+
+var acters =["mahi", 'opu',"sabana","kopila"];
+console.log(acters[3]);
+console.log(acters.length);
+console.log(friendsAge.length);
