dc615cacb906d2d81f3424dcfb7f8f56ba42f294
dev-rahim <rahimuddin5915@gmail.com>
2022-02-08 21:31:10 +0600

    Added array-index.js

---
+++ array-index.js
@@ -0,0 +1,10 @@
+var friendsAge = [ 11, 13, 15, 52, 52, 55, 52, 85, 865, 52, ];
+console.log( friendsAge[ 2 ] );
+
+var acters =["mahi", 'opu',"sabana","kopila"];
+
+var sabanaIndex=acters.indexOf('sabana');
+console.log(sabanaIndex);
+
+ acters[1]="APU BISSAS";
+console.log(acters[1]);
