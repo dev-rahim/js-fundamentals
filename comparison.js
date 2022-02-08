31e5ed1f627e2d9d556971be04baac2d160fbc7a
dev-rahim <rahimuddin5915@gmail.com>
2022-02-08 22:06:42 +0600

    Exploring conditional oparetors

---
+++ comparison.js
@@ -0,0 +1,12 @@
+console.log( 10 > 5 );
+console.log( 10 < 5 );
+console.log( 5 == 6 );
+console.log( 6 == 6 );
+console.log( 6 != 6 );
+
+var myLove = 100;
+var herLove = 20;
+console.log( myLove == herLove );
+
+console.log( studyGood || goodResult );
+console.log( studyGood && goodResult );
