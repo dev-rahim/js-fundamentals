f3781d0c17e6069a0b4d808d155840b4eb1d8340
dev-rahim <rahimuddin5915@gmail.com>
2022-02-08 20:57:09 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+// Number 
+var watchPrice=130;
+watchPrice=150;
+watchPrice ++;
+console.log(watchPrice);
+
+// String
+var userName='hellow world'
+
+// Boolean
+var isTrue=true;
+
+// integer: 12345678
+// float : 1.2 23.23 25.2
