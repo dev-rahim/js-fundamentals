4a8688e987fda5fbed2227c764bb65afbdab2fdd
dev-rahim <rahimuddin5915@gmail.com>
2022-02-08 22:20:38 +0600

    Exploring condition

---
+++ conditional.js
@@ -0,0 +1,16 @@
+if ( 10 > 9 ) {
+    console.log( "it's true" );
+}
+var iphonPrice = 70000;
+var myBudget = 80000;
+if ( iphonPrice < myBudget ) {
+    console.log( "I phone dea futani marbo" );
+}
+
+var chikenPrice = 100;
+var myMoney = 750;
+if ( chikenPrice < myMoney ) {
+    console.log( "Eat Chicken" );
+} else {
+    console.log( "Eat Lenten soup" );
+}
