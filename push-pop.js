f3448e4cef7fb918f0080e239e1294382ca9cdec
dev-rahim <rahimuddin5915@gmail.com>
2022-02-08 21:42:36 +0600

    Exploring push pop shift unshif function

---
+++ push-pop.js
@@ -0,0 +1,22 @@
+// add an element in Array
+var lastBench = [ "kalam", 'balam', "salam", "danesh" ];
+lastBench.push( "Rahim" );
+lastBench.push( "Rahim 1" );
+lastBench.push( "Rahim 2" );
+console.log( lastBench );
+
+// remove an element in Array
+lastBench.pop();
+lastBench.pop();
+lastBench.pop();
+lastBench.pop();
+console.log( lastBench );
+
+//js array remove first item from array
+lastBench.shift();
+console.log( lastBench );
+
+
+//js array add first item from array
+lastBench.unshift( "nasir" );
+console.log( lastBench );
